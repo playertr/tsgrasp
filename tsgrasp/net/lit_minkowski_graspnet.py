1d4a707fedc34ae3889d54b46b538f7c5dd3dd99
Tim Player <playert@oregonstate.edu>
2021-09-18 12:25:05 -0700

    Remove accuracy logging to debug OOM error

--- tsgrasp/net/lit_minkowski_graspnet.py
+++ tsgrasp/net/lit_minkowski_graspnet.py
@@ -70,13 +70,13 @@ class LitMinkowskiGraspNet(pl.LightningModule):
 
         return {'loss': loss, 'pt_preds': pt_preds, 'pt_labels': pt_labels}
 
-    def training_step_end(self, outputs):
-        self.train_pt_acc(outputs['pt_preds'], outputs['pt_labels'].int())
-        self.log('train_pt_acc', self.train_pt_acc)
+    # def training_step_end(self, outputs):
+    #     self.train_pt_acc(outputs['pt_preds'], outputs['pt_labels'].int())
+    #     self.log('train_pt_acc', self.train_pt_acc)
 
-    def validation_step_end(self, outputs):
-        self.val_pt_acc(outputs['pt_preds'], outputs['pt_labels'].int())
-        self.log('val_pt_acc', self.val_pt_acc)
+    # def validation_step_end(self, outputs):
+    #     self.val_pt_acc(outputs['pt_preds'], outputs['pt_labels'].int())
+    #     self.log('val_pt_acc', self.val_pt_acc)
 
     def _epoch_end(self, outputs, stage=None):
         if stage:
