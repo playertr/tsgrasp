db83899357679a9a5b13e816cc8f3d08e0452b4f
Tim Player <playert@oregonstate.edu>
2021-11-09 22:30:50 -0800

    Remove sampler

--- tsgrasp/data/lit_acronymvid.py
+++ tsgrasp/data/lit_acronymvid.py
@@ -20,14 +20,23 @@ class LitAcronymvidDataset(pl.LightningDataModule):
             self.dataset_val   = AcronymVidDataset(self.data_cfg, split="test")
 
     def train_dataloader(self):
-        return DataLoader(self.dataset_train, batch_size=self.batch_size, num_workers=self.num_workers, collate_fn=minkowski_collate_fn, sampler=RandomSampler(self.dataset_train, int(len(self.dataset_train) / self.data_cfg.subset_factor)))
+        return DataLoader(self.dataset_train, 
+        batch_size=self.batch_size, 
+        num_workers=self.num_workers, 
+        collate_fn=minkowski_collate_fn, 
+        # sampler=RandomSampler(self.dataset_train, int(len(self.dataset_train) / self.data_cfg.subset_factor))
+        )
 
     def val_dataloader(self):
-        return DataLoader(self.dataset_val, batch_size=self.batch_size, num_workers=self.num_workers, collate_fn=minkowski_collate_fn,  sampler=RandomSampler(self.dataset_val, int(len(self.dataset_val) / self.data_cfg.subset_factor)))
+        return DataLoader(self.dataset_val, batch_size=self.batch_size, num_workers=self.num_workers, collate_fn=minkowski_collate_fn,  
+        # sampler=RandomSampler(self.dataset_val, int(len(self.dataset_val) / self.data_cfg.subset_factor))
+        )
 
     def test_dataloader(self):
         # TODO: DON'T USE VAL DATASET FOR TESTING!
-        return DataLoader(self.dataset_val, batch_size=self.batch_size, num_workers=self.num_workers, collate_fn=minkowski_collate_fn, sampler=RandomSampler(self.dataset_val, int(len(self.dataset_val) / self.data_cfg.subset_factor)))
+        return DataLoader(self.dataset_val, batch_size=self.batch_size, num_workers=self.num_workers, collate_fn=minkowski_collate_fn, 
+        # sampler=RandomSampler(self.dataset_val, int(len(self.dataset_val) / self.data_cfg.subset_factor))
+        )
 
     def prepare_data(self):
         files = os.listdir(self.data_cfg.dataroot)
@@ -35,28 +44,28 @@ class LitAcronymvidDataset(pl.LightningDataModule):
             raise FileNotFoundError(f"Dataroot <{self.data_cfg.dataroot}> not populated with data files. Download or generate dataset.")
 
 # https://discuss.pytorch.org/t/new-subset-every-epoch/85018
-class RandomSampler(torch.utils.data.Sampler):
-    def __init__(self, data_source, num_samples=None):
-        self.data_source = data_source
-        self._num_samples = num_samples
+# class RandomSampler(torch.utils.data.Sampler):
+#     def __init__(self, data_source, num_samples=None):
+#         self.data_source = data_source
+#         self._num_samples = num_samples
 
-        if not isinstance(self.num_samples, int) or self.num_samples <= 0:
-            raise ValueError(
-                "num_samples should be a positive integer "
-                "value, but got num_samples={}".format(self.num_samples)
-            )
+#         if not isinstance(self.num_samples, int) or self.num_samples <= 0:
+#             raise ValueError(
+#                 "num_samples should be a positive integer "
+#                 "value, but got num_samples={}".format(self.num_samples)
+#             )
 
-    @property
-    def num_samples(self):
-        # dataset size might change at runtime
-        if self._num_samples is None:
-            return len(self.data_source)
-        return self._num_samples
+#     @property
+#     def num_samples(self):
+#         # dataset size might change at runtime
+#         if self._num_samples is None:
+#             return len(self.data_source)
+#         return self._num_samples
 
-    def __iter__(self):
-        n = len(self.data_source)
-        return iter(torch.randperm(n, dtype=torch.int64)[: self.num_samples].tolist())
+#     def __iter__(self):
+#         n = len(self.data_source)
+#         return iter(torch.randperm(n, dtype=torch.int64)[: self.num_samples].tolist())
 
-    def __len__(self):
-        return self.num_samples
+#     def __len__(self):
+#         return self.num_samples
 
