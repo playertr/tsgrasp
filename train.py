c9fc365cfdabdf053835c7411e71d966b04ccfdd
Tim Player <playert@oregonstate.edu>
2021-11-11 14:19:40 -0800

    Try removing wandb.init()

--- train.py
+++ train.py
@@ -1,13 +1,13 @@
 import hydra
 from omegaconf import DictConfig
 from tsgrasp.training.trainer import Trainer
-import wandb
+# import wandb
 
 @hydra.main(config_path="conf", config_name="config")
 def train(cfg : DictConfig):
 
-    if cfg.training.use_wandb:
-        wandb.init()
+    # if cfg.training.use_wandb:
+    #     wandb.init()
         
     trainer = Trainer(cfg)
     trainer.train()
