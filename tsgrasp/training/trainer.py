b333e33511e3d3eb9022511e7eb324cd4a776dcf
Tim Player <timrplayer@gmail.com>
2021-12-28 19:53:22 -0800

    Increase overfit_batches

--- tsgrasp/training/trainer.py
+++ tsgrasp/training/trainer.py
@@ -64,7 +64,7 @@ class Trainer:
         kwargs = dict(strategy=DDPPlugin(find_unused_parameters=True)) if cfg.training.gpus > 1 else {}
 
         if True:
-            kwargs.update(dict(overfit_batches=1, check_val_every_n_epoch=100))
+            kwargs.update(dict(overfit_batches=5, check_val_every_n_epoch=100))
         from pytorch_lightning.profiler import PyTorchProfiler
         profiler = PyTorchProfiler(filename="tsgrasp.prof", with_stack=True, with_modules=True)
         self.trainer = pl.Trainer(
