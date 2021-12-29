e8d0ee3b3a6c907b6058253b9768b6f180b5ca3a
Tim Player <timrplayer@gmail.com>
2021-12-28 21:45:28 -0800

    Remove profiler

--- tsgrasp/training/trainer.py
+++ tsgrasp/training/trainer.py
@@ -78,7 +78,7 @@ class Trainer:
             callbacks=_callbacks,
             max_epochs=cfg.training.max_epochs,
             resume_from_checkpoint=ckpt,
-            profiler=profiler,
+            # profiler=profiler,
             **kwargs
         )
 
