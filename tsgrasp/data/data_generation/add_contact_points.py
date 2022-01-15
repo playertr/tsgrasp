fd5b29e3fc8d2696eb74008ebb9334f7ddd797bc
Tim Player <playert@oregonstate.edu>
2022-01-14 22:10:20 -0800

    Add include guards

--- tsgrasp/data/data_generation/add_contact_points.py
+++ tsgrasp/data/data_generation/add_contact_points.py
@@ -8,16 +8,21 @@ import os
 import numpy as np
 import h5py
 from tqdm import tqdm
-from multiprocessing import Pool, cpu_count
+from multiprocessing import Pool
 import csv
 from omegaconf import DictConfig
 from functools import partial
 import sys
 from rtree.exceptions import RTreeError
 
-def append_grasp_info(h5_path, mesh_root):
+def append_grasp_info(h5_path, mesh_root, cfg):
     """Generate contact points and append them to a single file."""
-    global load_mesh, load_grasps, grasps_contact_info
+    if cfg.CONTACT_GRASPNET_REPO not in sys.path:
+        sys.path.insert(0, cfg.CONTACT_GRASPNET_REPO)
+    from tools.create_contact_infos import grasps_contact_info
+    if cfg.ACRONYM_REPO not in sys.path:
+        sys.path.insert(0, cfg.ACRONYM_REPO)
+    from acronym_tools import load_mesh, load_grasps
 
     # Check to see whether this file has been modified yet
     with h5py.File(h5_path) as ds:
@@ -29,20 +34,20 @@ def append_grasp_info(h5_path, mesh_root):
         obj_mesh = load_mesh(filename=h5_path, mesh_root_dir=mesh_root)
     except ValueError as e:
         print(e)
-        breakpoint()
+        # breakpoint()
         return False
 
     T, success = load_grasps(filename=h5_path)
 
     try:
-        contact_dicts = grasps_contact_info(grasp_tfs=T, successfuls=success, obj_mesh=obj_mesh, check_collisions=True)
+        contact_dicts = grasps_contact_info(grasp_tfs=T, successfuls=success, obj_mesh=obj_mesh, check_collisions=False)
     except AttributeError as e: # NoneType object has no attribute 'faces'
         print(e)
-        breakpoint()
+        # breakpoint()
         return False
     except RTreeError as e: # obscure, maybe memory-related error?
         print(e)
-        breakpoint()
+        # breakpoint()
         return False
 
     # `contact_dicts` is a list of dictionaries, with one dict per grasp.
@@ -51,6 +56,7 @@ def append_grasp_info(h5_path, mesh_root):
     # However, we want to associate every grasp with a set of contact points in the h5 file.
 
     # The following trickery with `transform_idxs` constructs a dictionary of the indices corresponding to each transform. For speed, dictionaries are used to look up the correspondences. Because ndarrays are an unhashable type, the `.tobytes()` method is called on them, creating a byte representation. This technique is potentially brittle: it assumes that `grasps_contact_info` will not break the bitwise representation of the grasp transformation, which might not be the case with all versions of ContactGraspNet or all systems.
+    print(f"{len(contact_dicts)} contact points identified.")
 
     transform_idxs = {}
     for i, d in enumerate(contact_dicts):
@@ -77,32 +83,28 @@ def add_contact_points(cfg: DictConfig):
     # for loading meshes and adding contact points.
     # We import this way so that we can specify the path to their repos in yaml.
     # We make them global so we're not passing expensive arguments between processes.
-    global grasps_contact_info, load_mesh, load_grasps
-    sys.path.insert(0, cfg.CONTACT_GRASPNET_REPO)
-    from tools.create_contact_infos import grasps_contact_info
-
-    sys.path.insert(0, cfg.ACRONYM_REPO)
-    from acronym_tools import load_mesh, load_grasps
 
 
     h5_paths = [os.path.join(cfg.DS_DIR, f) for f in os.listdir(cfg.DS_DIR) if f.endswith(".h5")]
 
     append_grasp = partial(append_grasp_info, 
-        mesh_root=cfg.MESH_DIR
+        mesh_root=cfg.MESH_DIR,
+        cfg = cfg
     )
 
     ## DEBUG
     # h5_paths = h5_paths[:50]
 
-    # successes = [append_grasp(path) for path in tqdm(h5_paths)]
-
-    with Pool(cfg.PROCESSES) as p:
-        successes = list(
-            tqdm(
-                p.imap_unordered(append_grasp, h5_paths),
-                total=len(h5_paths)
-            )
-    )
+    if cfg.PROCESSES == 0:
+        successes = [append_grasp(path) for path in tqdm(h5_paths)]
+    else:
+        with Pool(cfg.PROCESSES) as p:
+            successes = list(
+                tqdm(
+                    p.imap_unordered(append_grasp, h5_paths),
+                    total=len(h5_paths)
+                )
+        )
 
     print("Successful:")
     print(successes)
